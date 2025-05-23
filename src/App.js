c570d64050a6a7c2558c0583a183e454ef3bbc18
Minn97u <kmg09190@naver.com>
2025-05-23 09:05:54 +0900

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome MinGyu <code>src/App.js</code> and save to reload.
+          Welcome MinGyu <code>Whare</code> and save to reload.
         </p>
         <a
           className="App-link"
